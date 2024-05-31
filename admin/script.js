5328d05c76c7a944606c648bb8699ed658b1a74b
sanj2cool <67955810+sanj2cool@users.noreply.github.com>
2024-05-31 19:09:04 +0530

    Update script.js

--- admin/script.js
+++ admin/script.js
@@ -1,115 +1,6 @@
-function displayLeadData(
-  lead,
-  cols = false,
-  classs = "col-md-4", // Adjusted default class for 3 columns
-  tablecount = 3 // Default set to 3 tables
-) {
-  // Assuming 'lead' is an object with key-value pairs.
-  if (lead) {
-    // Remove 'call_history' from the lead object and filter out empty values
-    lead = Object.fromEntries(
-      Object.entries(lead).filter(
-        ([key, value]) =>
-          key !== "call_history" &&
-          value != null &&
-          value !== "" &&
-          value !== "0"
-      )
-    );
-
-    const container = document.createElement("div");
-    container.className = "container";
-
-    const row = document.createElement("div");
-    row.className = "row";
-    container.appendChild(row);
-
-    // Create columns and tables based on tablecount
-    const columns = [];
-    for (let i = 0; i < tablecount; i++) {
-      const col = document.createElement("div");
-      col.className = classs;
-      row.appendChild(col);
-
-      const table = document.createElement("table");
-      table.className = "table table-bordered";
-      col.appendChild(table);
-
-      columns.push(table);
-    }
-
-    const itemsPerTable = Math.ceil(Object.keys(lead).length / tablecount);
-    let currentTableIndex = 0;
-    let currentItemCount = 0;
-
-    Object.entries(lead).forEach(([key, value]) => {
-      key = cols ? cols[key] : key;
-      const tr = document.createElement("tr");
-      const th = document.createElement("th");
-      th.textContent = slugToText(key);
-      const td = document.createElement("td");
-
-      let cond = key.toLowerCase();
-
-      // Create clickable links for specific fields
-      if (cond.includes("address")) {
-        const a = document.createElement("a");
-        a.href =
-          "https://www.google.com/maps/search/" + encodeURIComponent(value);
-        a.textContent = value;
-        a.target = "_blank";
-        td.appendChild(a);
-      } else if (cond.includes("time")) {
-        td.textContent = timestampToHumanTime(parseInt(value) * 1000);
-      } else if (cond.includes("email")) {
-        const a = document.createElement("a");
-        a.href = `mailto:${value}`;
-        a.textContent = value;
-        a.target = "_blank";
-        td.appendChild(a);
-      } else if (cond.includes("phone")) {
-        const a = document.createElement("a");
-        a.href = `tel:${value}`;
-        a.textContent = value;
-        a.target = "_blank";
-        td.appendChild(a);
-      } else if (cond === "website" || cond.endsWith("_url")) {
-        const a = document.createElement("a");
-        a.href = value;
-        a.textContent = value;
-        a.target = "_blank";
-        td.appendChild(a);
-      } else if (cond === "is_facebook_ads") {
-        td.textContent = value === "1" ? "Yes" : "No";
-      } else {
-        td.textContent = value;
-      }
-
-      tr.appendChild(th);
-      tr.appendChild(td);
-
-      columns[currentTableIndex].appendChild(tr);
-      currentItemCount++;
-
-      if (currentItemCount >= itemsPerTable) {
-        currentTableIndex++;
-        currentItemCount = 0;
-      }
-    });
-
-    return container;
-  } else {
-    const alertDiv = document.createElement("div");
-    alertDiv.className = "alert alert-warning";
-    alertDiv.textContent = "No lead data available.";
-    return alertDiv;
-  }
-}
 function timestampToHumanTime(timestamp) {
-  // Create a new Date object from the timestamp
   const date = new Date(timestamp);
 
-  // Options for formatting the date and time
   const options = {
     year: "numeric",
     month: "long",
@@ -120,19 +11,16 @@ function timestampToHumanTime(timestamp) {
     hour12: true,
   };
 
-  // Format the date and time using the options
   return date.toLocaleString("en-US", options);
 }
 
 function displayLeadDataImproved(
   lead,
   cols = false,
-  classs = "col-md-4", // Adjusted default class for 3 columns
-  tablecount = 3 // Default set to 3 tables
+  classs = "col-md-4",
+  tablecount = 3
 ) {
-  // Assuming 'lead' is an object with key-value pairs.
   if (lead) {
-    // Remove 'call_history' from the lead object and filter out empty values
     lead = Object.fromEntries(
       Object.entries(lead).filter(
         ([key, value]) =>
@@ -142,7 +30,7 @@ function displayLeadDataImproved(
           value !== "0"
       )
     );
-    //items list for showing in three columns under the appointmnet section
+
     const ColItem1 = [
       "company_name",
       "category",
@@ -160,8 +48,6 @@ function displayLeadDataImproved(
       "bbb_profile",
       "fb_url",
       "fb_rating",
-      // "fb_likes",
-      // "fb_reviews",
       "is_facebook_ads",
       "is_adwords",
       "instagram_url",
@@ -180,13 +66,12 @@ function displayLeadDataImproved(
     row.className = "row";
     container.appendChild(row);
 
-    // Create columns and tables based on tablecount
     const columns = [];
     for (let i = 0; i < tablecount; i++) {
       const col = document.createElement("div");
       col.className = classs;
       row.appendChild(col);
-      //adding header just for 3 columns leads data
+
       if (tablecount === 3) {
         const head = document.createElement("div");
         head.className = "col text-center h4 mb-3";
@@ -215,16 +100,17 @@ function displayLeadDataImproved(
       key = cols ? cols[key] : key;
       const tr = document.createElement("tr");
       const th = document.createElement("th");
-      th.textContent = slugToText(key);
+
+      let capitalize = handleCapitalize(key);
+      th.textContent = capitalize ? capitalize : slugToText(key);
+
       const td = document.createElement("td");
 
       let cond = key.toLowerCase();
 
-      // Create clickable links for specific fields
       if (cond.includes("address")) {
         const a = document.createElement("a");
-        a.href =
-          "https://www.google.com/maps/search/" + encodeURIComponent(value);
+        a.href = "https://www.google.com/maps/search/" + encodeURIComponent(value);
         a.textContent = value;
         a.target = "_blank";
         td.appendChild(a);
@@ -242,14 +128,14 @@ function displayLeadDataImproved(
         a.textContent = value;
         a.target = "_blank";
         td.appendChild(a);
-      } else if (cond === "website" || cond.endsWith("_url")) {
+      } else if (cond === "website" || cond.endsWith("_url") || cond === "bbb_profile") {
         const a = document.createElement("a");
         a.href = value;
         a.textContent = value;
         a.target = "_blank";
         td.appendChild(a);
       } else if (cond === "is_facebook_ads") {
-        td.textContent = value === "1" ? "XYZ" : "";
+        td.textContent = value === "1" ? "Yes" : "No";
       } else {
         td.textContent = value;
       }
@@ -265,7 +151,6 @@ function displayLeadDataImproved(
           currentItemCount = 0;
         }
       } else {
-        //populating table by checking the provided arrays for col1, col2, col3
         if (ColItem1.includes(cond)) {
           columns[0].appendChild(tr);
         } else if (ColItem2.includes(cond)) {
@@ -275,30 +160,14 @@ function displayLeadDataImproved(
           }
           columns[1].appendChild(tr);
         } else if (ColItem3.includes(cond)) {
-          //modifing the strings
           if (cond === "fb_rating") {
             th.textContent = "FB Stats";
-            td.textContent = lead["first_name"] + " " + lead["last_name"];
-            td.textContent =
-              lead["fb_likes"] +
-              " Likes –" +
-              lead["fb_rating"] +
-              " Rating (from " +
-              lead["fb_reviews"] +
-              " Reviews)";
-          }
-          if (
-            cond === "is_facebook_ads" ||
-            cond === "bing_ads" ||
-            cond === "twitter_ads" ||
-            cond === "linkedin_ads"
-          ) {
-            td.textContent = value === "1" ? "Yes" : "";
-          }
-          if (cond === "is_adwords") {
+            td.textContent = `${lead["fb_likes"]} Likes – ${lead["fb_rating"]} Rating (from ${lead["fb_reviews"]} Reviews)`;
+          } else if (["is_facebook_ads", "bing_ads", "twitter_ads", "linkedin_ads"].includes(cond)) {
+            td.textContent = value === "1" ? "Yes" : "No";
+          } else if (cond === "is_adwords") {
             th.textContent = "Google Ads";
-            td.textContent =
-              "is_adwords(" + (value === "1" ? "Yes" : "No") + ")";
+            td.textContent = value === "1" ? "Yes" : "No";
           }
           columns[2].appendChild(tr);
         }
@@ -313,15 +182,22 @@ function displayLeadDataImproved(
     return alertDiv;
   }
 }
+
 function slugToText(slug) {
-  // Replace hyphens with spaces and split the string into words
-  let words = slug.replace(/-/g, " ").replace(/_/g, " ").split(" ");
+  let words = slug.replace(/[-_]/g, " ").split(" ");
+  let capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+  return capitalizedWords.join(" ");
+}
 
-  // Capitalize the first letter of each word
-  let capitalizedWords = words.map(
-    (word) => word.charAt(0).toUpperCase() + word.slice(1)
-  );
-
-  let readableText = capitalizedWords.join(" ");
-  return readableText;
+function handleCapitalize(str) {
+  if (str === "bbb_profile") {
+    let parts = str.split('_');
+    if (parts.length !== 2) {
+      return false;
+    }
+    let firstPart = parts[0].toUpperCase();
+    let secondPart = parts[1].charAt(0).toUpperCase() + parts[1].slice(1).toLowerCase();
+    return `${firstPart} ${secondPart}`;
+  }
+  return false;
 }
